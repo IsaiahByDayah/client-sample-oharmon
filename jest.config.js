94f78a30b248f49e2cec78ff026adfc47b71d97f
Isaiah Smith <jordankid93@gmail.com>
2025-01-24 14:14:54 -0800

    chore: misc cleanup

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    verbose: true,
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    moduleDirectories: ["node_modules", "<rootDir>"],
-    moduleNameMapper: {
-        "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
-    },
-}
